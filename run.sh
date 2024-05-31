3eb527d1897b62a615c4a7d0f1f74ee9966d882f
iERROR0098 <a.fshinb560@gmail.com>
2024-05-31 01:17:27 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,14 +8,27 @@ sudo apt-get install -y haproxy vnstat
 # Multi-line text to append
 text_to_append=$(cat <<EOF
 
-listen front
-    mode tcp
-    bind *:443
-    use_backend to443
 
-backend to443
-    mode tcp
-    server server1 37.27.80.144:443
+listen front
+ mode tcp
+ bind *:443
+
+ tcp-request inspect-delay 5s
+ tcp-request content accept if { req_ssl_hello_type 1 }
+ use_backend reality if { req.ssl_sni -m end app.hubspot.com | www.sephora.com | cdn.discordapp.com | icloud.com }
+
+backend reality
+ mode tcp
+ server ru 86.159.231.30:6000 send-proxy
+
+frontend http_front
+ bind *:80
+ mode http
+ redirect location https://nextcloud.technologiewerk-qua.de code 301
+
+backend http_back
+ mode http
+ balance roundrobin
 
 
 EOF
@@ -25,3 +38,26 @@ EOF
 echo "$text_to_append" >> "/etc/haproxy/haproxy.cfg"
 
 sudo systemctl restart haproxy
+
+# Check firewall status
+sudo ufw status | grep -q "Status: active"
+ufw_status=$?
+
+if [ $ufw_status -eq 0 ]; then
+  # Firewall is active, check if port 443 is open
+  sudo ufw status | grep -q "443"
+  port_443_status=$?
+
+  if [ $port_443_status -ne 0 ]; then
+    # Port 443 is closed, open ports 22, 80, and 443
+    sudo ufw allow 22
+    sudo ufw allow 80
+    sudo ufw allow 443
+    sudo ufw allow 8008
+    echo "Opened ports 22, 80, and 443"
+  else
+    echo "Port 443 is already open"
+  fi
+else
+  echo "Firewall is not active"
+fi
